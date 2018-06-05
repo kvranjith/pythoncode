24d7e3ab2e05e221898f001b0853ded95044aa48
unknown <ranjith.v@8kmiles.local>
2018-06-05 10:49:02 +0530

    New code

---
+++ sts.py
@@ -0,0 +1,9 @@
+import boto3
+client = boto3.client('ec2',region_name='us-east-2')
+response = client.describe_instances()
+for ins in response['Reservations']:
+    for vol in ins['Instances']:
+        test = vol['InstanceId'],vol['State']['Name']
+        print test
+
+
