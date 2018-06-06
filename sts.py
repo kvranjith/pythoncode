b43bc8a7c010f1f52c02493430293153e4d8a5d4
unknown <ranjith.v@8kmiles.local>
2018-06-06 12:35:53 +0530

    code modified with random id

--- sts.py
+++ sts.py
@@ -1,11 +1,13 @@
 import boto3
 import uuid
 
+random_id = str(uuid.uuid4())
+
 client = boto3.client('ec2',region_name='us-east-2')
 response = client.describe_instances()
 for ins in response['Reservations']:
     for vol in ins['Instances']:
         test = vol['InstanceId'],vol['State']['Name']
-        print test
+        print random_id,'-',test
 
 
